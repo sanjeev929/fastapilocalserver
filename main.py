7a71af05d3b65927c40287a538d97a98015ef75b
Sanjeev <123805942+sanjeev929@users.noreply.github.com>
2024-04-22 06:35:02 +0530

    req

--- main.py
+++ main.py
@@ -1382,4 +1382,4 @@ async def deletewtaDetailsById(item_id: int):
         return {"msg": f"Successfully deleted item in {item_id}"}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8182, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8182, reload=True)
